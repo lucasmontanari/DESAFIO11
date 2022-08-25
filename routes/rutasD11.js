fc746ab6ca30e2838f99517a4606952169efbcb6
Lucas Montanari <106715446+lucasmontanari@users.noreply.github.com>
2022-08-24 21:32:22 -0300

    Add files via upload

---
+++ routes/rutasD11.js
@@ -0,0 +1,36 @@
+import { fork } from 'child_process';
+
+function info(req, res) {
+    const result = {
+        argumentos: `${process.argv.slice(2)}`,
+        plataforma: process.platform,
+        versionNode: process.version,
+        usoMemoria: process.memoryUsage().rss,
+        path: process.execPath,
+        processId: process.pid,
+        carpetaProyecto: process.cwd(),
+    };
+    res.status(200).render('info', result);
+}
+
+function randoms(req, res) {
+    let cantidad = req.query.cant;
+
+    if (!cantidad) {
+        cantidad = 1e8;
+    }
+    const forked = fork('controllers/generarNumerosRandom.js');
+
+    forked.on('message', (resultado) => {
+        if (resultado === 'listo') {
+            forked.send(cantidad);
+        } else {
+            res.status(200).json({ resultado: resultado });
+        }
+    });
+}
+
+export default {
+    info,
+    randoms
+};
